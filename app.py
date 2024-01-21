0aa390d389495612116098b0b5db80655804de1d
slinhomes <142390550+slinhomes@users.noreply.github.com>
2024-01-21 18:19:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 import streamlit as st
+import hmac
 import add_requirements
 import search_requirements
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("Password incorrect, please try again.")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# Main Streamlit app starts here
+st.button("Click me")
+
 
 # Initialize multi-page app setup
 def main():
