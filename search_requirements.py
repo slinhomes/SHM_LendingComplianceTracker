272c20d882ed4016c198c9488ae1df8152599dcc
slinhomes <142390550+slinhomes@users.noreply.github.com>
2024-01-22 12:57:34 +0000

    Update search_requirements.py

--- search_requirements.py
+++ search_requirements.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 def show():
-    st.write("Welcome to the Search for Existing Requirements Page")
-    # Your code for this page goes here
+    st.write("Welcome to the Search for Existing Compliance Requirements Page")
+    st.caption("Please note that this site is currently under development.")
+    # More code
