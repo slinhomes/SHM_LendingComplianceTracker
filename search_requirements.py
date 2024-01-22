b8c8bd260e3b275032b9ae91cc8d2450139c79bc
slinhomes <142390550+slinhomes@users.noreply.github.com>
2024-01-22 13:12:57 +0000

    Update search_requirements.py

--- search_requirements.py
+++ search_requirements.py
@@ -1,6 +1,39 @@
 import streamlit as st
 
+# Function to connect to the Azure SQL database
+def create_connection():
+    server = 'studenthomesmgmt.database.windows.net'
+    database = 'PortfolioManagement'
+    username = st.secrets['db_username']
+    password = st.secrets['db_password']
+    driver = '{ODBC Driver 17 for SQL Server}'  # Adjust the driver if needed
+    conn = pyodbc.connect(
+        f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
+    return conn
+
 def show():
     st.write("Welcome to the Search for Existing Compliance Requirements Page")
     st.caption("Please note that this site is currently under development.")
+    st.markdown("---")  # Page breaker
+
+    # Connect to the database and fetch property addresses and IDs
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT DISTINCT Asset_Address, Dwelling_Address, Lender FROM SHMLendingCompliance")
+    rows = cursor.fetchall()
+
+    # Separate the fetched data into individual lists
+    asset_addresses = [row[0] for row in rows]
+    dwelling_addresses = [row[1] for row in rows]
+    lenders = [row[2] for row in rows]
+
+    # Search bars
+    selected_asset_address = st.selectbox("Property", [""] + asset_addresses)
+    selected_dwelling_address = st.selectbox("Detailed Address", [""] + dwelling_addresses)
+    st.caption("Note: only select this field if you are looking for dwelling level requirements.")
+    selected_lender = st.selectbox("Lender", [""] + lenders)
+
     # More code
+
+
+
